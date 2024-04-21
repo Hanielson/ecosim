2ecd6c5f5bb530e9223b1ac4657282f590a9fdac
Haniel Schirmer <81054500+Hanielson@users.noreply.github.com>
2024-04-21 02:54:30 +0000

    Move function + death of rabbit implemented

--- src/main.cpp
+++ src/main.cpp
@@ -176,11 +176,89 @@ int die(entity_t& entity , int x_pos , int y_pos){
         // CRITICAL SECTION
         std::unique_lock<std::mutex> ul(end_task);
         entity_grid.at(x_pos).at(y_pos) = entity_t(entity_type_t::empty , 0 , 0);
+        // END OF CRITICAL SECTION
         return 0;
     }
     else{
         return 1;
     }
+}
+
+// Function that updates the position of the Herbivores and Carnivores, according to each own rules
+// Returns pointer to entity in new position
+entity_t* move(entity_t& entity , int x_pos , int y_pos){
+
+    // Instantiation of Random Number Generator Engine
+    std::random_device rd;
+    std::default_random_engine generator(rd());
+    std::uniform_int_distribution percentage(1 , 100);
+
+    // Entity Movements
+    if( ( (entity.type == entity_type_t::herbivore) && (percentage(generator) <= 70) )
+      ||( (entity.type == entity_type_t::carnivore) && (percentage(generator) <= 50) ) ){
+        
+        // Is there ate least ONE valid position?
+        bool valid_count = false;
+
+        // Array identifying each positions as valid or not
+        // true == valid && false == invalid
+        bool pos[3][3] = {false};
+
+        // There's an 3x3 Grid centered on (x_pos , y_pos)
+        // We can index each square of the grid based on (x_pos , y_pos) + (-1/0/1 , -1/0/1)
+        std::uniform_int_distribution rand_cell(-1 , 1);
+
+        // CRITICAL SECTION
+        // No one can affect the grid while the plant grows somewhere
+        std::unique_lock<std::mutex> mut(end_task);
+
+        // Updates array with valid values
+        // Essa parte aqui tá PODRÍFERA, mas vai ter que servir
+        for(int x_mod = -1 ; x_mod <= 1 ; ++x_mod){
+            for(int y_mod = -1 ; y_mod <= 1 ; ++y_mod){
+                // Checks if Position is outside the grid
+                if( ( (x_pos + x_mod) < 0 ) || ( (x_pos + x_mod) >= (int)NUM_ROWS ) 
+                  ||( (y_pos + y_mod) < 0 ) || ( (y_pos + y_mod) >= (int)NUM_ROWS )){
+                    continue;
+                }
+
+                entity_type_t adjacent_type = entity_grid.at(x_pos + x_mod).at(y_pos + y_mod).type;
+                if( ( (entity.type == entity_type_t::herbivore) && (adjacent_type == entity_type_t::empty) ) 
+                  ||( (entity.type == entity_type_t::carnivore) && (adjacent_type != entity_type_t::plant) ) ){
+                    pos[x_mod + 1][y_mod + 1] = true;
+                    valid_count = true;
+                };
+            }
+        }
+
+        // If there is not a single valid position, the function returns
+        if(!valid_count){
+            // END OF CRITICAL SECTION
+            return nullptr;
+        }
+
+        // Position to be acted upon
+        int x_act = 0;
+        int y_act = 0;
+        do{
+            x_act = rand_cell(generator);
+            y_act = rand_cell(generator);
+            printf("x_act : %d /// y_act : %d\n" , x_act , y_act);
+        }while(pos[x_act + 1][y_act + 1] == false);
+
+        // Herbivore Movement
+        if(entity.type == entity_type_t::herbivore){
+            entity_grid.at(x_pos + x_act).at(y_pos + y_act) = entity_t(entity.type , (entity.energy - 5) , entity.age);
+            entity_grid.at(x_pos).at(y_pos) = entity_t(entity_type_t::empty , 0 , 0);
+        }
+        // END OF CRITICAL SECTION
+
+        return &entity_grid.at(x_pos + x_act).at(y_pos + y_act);
+
+    }
+
+    return nullptr;
+
 }
 
 // Function action(entity_t& , int x_pos , int y_pos)
@@ -200,6 +278,25 @@ int action(entity_t& entity , int x_pos , int y_pos , MyBarrier& my_barrier){
             printf("Plant is growing\n");
             grow(x_pos , y_pos , generator);
             ++entity.age;
+        }
+    }
+
+    // Herbivore Actions
+    if(entity.type == entity_type_t::herbivore){
+        if(die(entity , x_pos , y_pos)){
+            // Entity moves to a new position
+            // The address of the entity in the new position is returned
+            entity_t* new_pos = move(entity , x_pos , y_pos);
+
+            // If no movement was made and we need to operate through the Entity reference
+            // Otherwise, we need to operate through the returned address
+            // Algoritmo bem podre, mas vai ter que servir MESMO
+            if(new_pos == nullptr){
+                ++entity.age;
+            }
+            else{
+                ++(new_pos->age);
+            }
         }
     }
 
